d32d65983e218dd913694058b9724c6033d25c42
reallywasi <118682540+reallywasi@users.noreply.github.com>
2023-07-24 23:02:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// let string = "";
+// let buttons = document.querySelectorAll(".button"); // sab ko select karega jinki class button hai
+
+// Array.from(buttons).forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     if (e.target.innerHTML == "=") {
+//       string = eval(string);
+//       document.querySelector("input").value = string;
+//     } else {
+//       console.log(e.target);
+//       string = string + e.target.innerHTML;
+//       document.querySelector("input").value = string;
+//     }
+//   });
+// });
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
